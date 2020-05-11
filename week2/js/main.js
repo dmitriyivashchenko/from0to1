dd41298dd33f547a42d69b58f2bb933181d17e00
dmitriyivashchenko <markfederal4@gmail.com>
2020-05-11 20:31:51 +0300

    lesson 7 done (adaptive)

--- week2/js/main.js
+++ week2/js/main.js
@@ -9,8 +9,38 @@ $(function(){
       slidesToShow: 4,
       slidesToSctoll: 1,
       prevArrow: ' <button class="slick-arrow slick-prev"><img src="images/icons/slider-left-arrow.svg" alt=""></button>',
-      nextArrow: ' <button class="slick-arrow slick-next"><img src="images/icons/slider-right-arrow.svg" alt=""></button>'
+      nextArrow: ' <button class="slick-arrow slick-next"><img src="images/icons/slider-right-arrow.svg" alt=""></button>',
+      responsive:[
+        {
+          breakpoint: 910,
+          settings: {
+            slidesToShow: 3,
+          }
+        },
+        {
+          breakpoint: 681,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 455,
+          settings: {
+            slidesToShow: 1,
+          }
+        },
+      ]
       });
 
+    $('.header__menu-btn').on('click', function(){
+      $('.header__menu > ul').slideToggle();
+    });
+
+    $('.header__drop-down.drop-down').on('click', function(){
+      $(this).children('.drop-down__list').toggleClass('active');
+      $(this).children('.drop-down__link').toggleClass('active');
+    });
+
+    $('.header__drop-down.drop-down')
   });
-          
+
