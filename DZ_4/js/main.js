32e5e595e2d23e48c31185364984824f5332b91a
dmitriyivashchenko <markfederal4@gmail.com>
2020-05-12 20:56:18 +0300

    dz 4 done

--- DZ_4/js/main.js
+++ DZ_4/js/main.js
@@ -5,20 +5,24 @@ $(function(){
   //   dots: true
   // });
   $('.slider__inner').slick({
-    // slidesToShow: 1,
+    slidesToShow: 1,
     // slidesToScroll: 1,
     arrows: true,
     fade: false,
-    asNavFor: '.slider__thumbnails',
     autoplay: false
   });
   $('.slider__thumbnails').slick({
-    slidesToShow: 6,
-    // slidesToScroll: 12,
     asNavFor: '.slider__inner',
+    slidesToShow: 6,
+    rows: 1,
+    slidesPerRow: 6,
     variableWidth: true,
     arrows: false,
-    focusOnSelect: true
+    focusOnSelect: true,
+  });
+
+  $('.header__menu-btn').on('click', function(){
+    $('.header__menu ul').slideToggle();
   });
 });
         
